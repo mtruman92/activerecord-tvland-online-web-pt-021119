ccd70bb8d1238b2d7f11785faf509116a6a14b3a
Mickae Truman <mtruman@nyit.edu>
2019-05-14 22:53:09 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,8 @@
 class Actor < ActiveRecord::Base
+  has_many :characters 
+  has_many :shows, through: :characters
   
+  def full_name
+    full_name = Actor.first_name + " " + Actor.last_name
+  end
 end
