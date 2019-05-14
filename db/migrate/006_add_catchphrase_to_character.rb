8312063e59966cf5238dac984d076f847be98e14
Mickae Truman <mtruman@nyit.edu>
2019-05-14 23:35:54 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -1,6 +1,6 @@
-class AddCatchPhraseToCharacter
+class AddCatchPhraseToCharacter < ActiveRecord::Migration[4.2]
    
     def change
-      add_column :character, :catchphrase, :string
+      add_column :characters, :catchphrase, :string
     end
 end 
