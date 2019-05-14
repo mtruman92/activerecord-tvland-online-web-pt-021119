1b3ee8351636198f02a5766b029faca788b35767
Mickae Truman <mtruman@nyit.edu>
2019-05-14 23:28:25 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,7 @@
 class Character < ActiveRecord::Base
       belongs_to :show 
       
-     def change
-      add_column :catchphrase
-     end 
+     
      
      def say_that_thing_you_say
        Character.catchphrase
